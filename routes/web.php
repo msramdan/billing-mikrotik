5f5687aaa88764b1511daa79ca999202bb51f3b8
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-08-24 22:32:35 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth', 'web'])->group(function () {
+    Route::get('/', fn () => view('dashboard'));
+    Route::get('/dashboard', fn () => view('dashboard'));
+
+    Route::get('/profile', App\Http\Controllers\ProfileController::class)->name('profile');
+
+    Route::resource('users', App\Http\Controllers\UserController::class);
+    Route::resource('roles', App\Http\Controllers\RoleAndPermissionController::class);
+});
+
+Route::middleware(['auth', 'permission:test view'])->get('/tests', function () {
+    dd('This is just a test and an example for permission and sidebar menu. You can remove this line on web.php, config/permission.php and config/generator.php');
+})->name('tests.index');
