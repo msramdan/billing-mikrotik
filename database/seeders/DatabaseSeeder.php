235485eb06dd28e08966d2e493bf5e2f6940a576
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-08-25 14:02:40 +0700

    add seeder privacy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleAndPermissionSeeder::class);
         $this->call(CompanySeeder::class);
         $this->call(WaGatewaySeeder::class);
+        $this->call(PrivacyPolicySeeder::class);
     }
 }
