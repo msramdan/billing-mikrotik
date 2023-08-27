f2341f9692521e99d7f3b6cbb46b9449ebbb98a3
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-08-27 12:51:08 +0700

    update

--- app/Http/Controllers/StatusrouterController.php
+++ app/Http/Controllers/StatusrouterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Statusrouter;
 use App\Http\Requests\{StoreStatusrouterRequest, UpdateStatusrouterRequest};
 use Yajra\DataTables\Facades\DataTables;
+use \RouterOS\Query;
 
 class StatusrouterController extends Controller
 {
@@ -15,6 +16,80 @@ class StatusrouterController extends Controller
 
     public function index()
     {
-        return view('statusrouters.show');
+        $client = setRoute();
+        $query = new Query('/ip/hotspot/user/print');
+        $hotspotuser = $client->query($query)->read();
+
+        $queryipcloud = new Query('/ip/cloud/print');
+        $ipcloud = $client->query($queryipcloud)->read();
+
+        $queryhotspotactive = new Query('/ip/hotspot/active/print');
+        $hotspotactive = $client->query($queryhotspotactive)->read();
+
+        $querypppsecrets = new Query('/ppp/secret/print');
+        $pppsecrets = $client->query($querypppsecrets)->read();
+
+        $querypppactive = new Query('/ppp/active/print');
+        $pppactive = $client->query($querypppactive)->read();
+
+        $queryidentity = new Query('/system/identity/print');
+        $identity = $client->query($queryidentity)->read();
+
+        $queryresource = new Query('/system/resource/print');
+        $resource = $client->query($queryresource)->read();
+
+        $querywaktu = new Query('/system/clock/print');
+        $waktu = $client->query($querywaktu)->read();
+
+        $querylicense = new Query('/system/license/print');
+        $license = $client->query($querylicense)->read();
+
+        $resource = json_encode($resource);
+        $resource = json_decode($resource, true);
+        $ipcloud = json_encode($ipcloud);
+        $ipcloud = json_decode($ipcloud, true);
+        $waktu = json_encode($waktu);
+        $waktu = json_decode($waktu, true);
+        $identity = json_encode($identity);
+        $identity = json_decode($identity, true);
+        $license = json_encode($license);
+        $license = json_decode($license, true);
+
+        $boardname = $resource['0']['board-name'];
+        if ($boardname == 'CHR') {
+            $level = $license['0']['level'];
+            $software = $license['0']['system-id'];
+        } else {
+            $level = $license['0']['nlevel'];
+            $software = $license['0']['software-id'];
+        }
+
+        $dataku = [
+            'hotspotuser' => count($hotspotuser),
+            'hotspotactive' => count($hotspotactive),
+            'pppsecrets' => count($pppsecrets),
+            'pppactive' => count($pppactive),
+            'identity' => $identity['0']['name'],
+            'publicrouter' => $ipcloud['0']['public-address'],
+            'cpuname' => $resource['0']['cpu'],
+            'cpucount' => $resource['0']['cpu-count'],
+            'frequency' => $resource['0']['cpu-frequency'],
+            'cpuload' => $resource['0']['cpu-load'],
+            'uptime' => $resource['0']['uptime'],
+            'boardname' => $resource['0']['board-name'],
+            'architecture' => $resource['0']['architecture-name'],
+            'freememory' => $resource['0']['free-memory'],
+            'totalmemory' => $resource['0']['total-memory'],
+            'freehdd' => $resource['0']['free-hdd-space'],
+            'totalhdd' => $resource['0']['total-hdd-space'],
+            'architecture' => $resource['0']['architecture-name'],
+            'version' => $resource['0']['version'],
+            'time' => $waktu['0']['time'],
+            'date' => $waktu['0']['date'],
+            'level' => $level,
+            'software' => $software,
+        ];
+
+        return view('statusrouters.show', $dataku);
     }
 }
--- resources/views/statusrouters/show.blade.php
+++ resources/views/statusrouters/show.blade.php
@@ -29,12 +29,103 @@
 
         <section class="section">
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-8">
                     <div class="card">
                         <div class="card-body">
-                            <div class="table-responsive">
-
+                            <div class="alert alert-info" role="alert">
+                                <h5 style="text-align: center;color:white">
+                                    Router Sudah Terkoneksi Saat Ini
+                                </h5>
                             </div>
+
+                            <div class="table-responsive">
+                                <table class="table table-striped" width="100%">
+                                    <thead>
+                                        <tr style="text-align: center">
+                                            <th colspan="6">Identitas Router : <?= $identity ?> (<?= $date ?> -
+                                                <?= $time ?> WIB) <br>
+                                                <b>Router Os : Version <?= $version ?> </b>
+
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td style="text-align: center;" width="5%">1</td>
+                                            <td width="15%">CPU Count</td>
+                                            <td width="30%"><?= $cpucount ?> Core &nbsp; ( Freq. : <?= $frequency ?> Mhz
+                                                )</td>
+                                            <td style="text-align: center;" width="5%">7</td>
+                                            <td width="15%">Router Uptime</td>
+                                            <td width="30%"><?= $uptime ?> </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="text-align: center;" width="5%">2</td>
+                                            <td width="15%">CPU Load</td>
+                                            <td width="30%"><?= $cpuload ?> % &nbsp; ( <?= $cpuname ?> )</td>
+                                            <td style="text-align: center;" width="5%">8</td>
+                                            <td width="15%">Dapat IP Public</td>
+                                            <td width="30%"><?= $publicrouter ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td style="text-align: center;" width="5%">3</td>
+                                            <td width="15%">Free Memory</td>
+                                            <td width="30%">
+                                                {{ formatBytes($freememory, 2) }} / {{ formatBytes($totalmemory, 2) }}
+                                            </td>
+                                            <td style="text-align: center;" width="5%">9</td>
+                                            <td width="15%">Hotspot Active</td>
+                                            <td width="30%"><?= $hotspotactive ?> Device</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="text-align: center;" width="5%">4</td>
+                                            <td width="15%">Free HDD</td>
+                                            <td width="30%">
+                                                {{ formatBytes($freehdd, 2) }} / {{ formatBytes($totalhdd, 2) }}
+                                            </td>
+                                            <td style="text-align: center;" width="5%">10</td>
+                                            <td width="15%">Users Hotspot</td>
+                                            <td width="30%"><?= $hotspotuser ?> Voucher</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="text-align: center;" width="5%">5</td>
+                                            <td width="15%">RouterBoard</td>
+                                            <td width="30%"><?= $boardname ?> &nbsp; ( <?= $architecture ?> )</td>
+                                            <td style="text-align: center;" width="5%">11</td>
+                                            <td width="15%">PPP Active</td>
+                                            <td width="30%"><?= $pppactive ?> Customer</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="text-align: center;" width="5%">6</td>
+                                            <td width="15%">Level Lisence</td>
+                                            <td width="30%"><?= $level ?> &nbsp; ( SW ID : <?= $software ?> )</td>
+                                            <td style="text-align: center;" width="5%">12</td>
+                                            <td width="15%">Secret PPP</td>
+                                            <td width="30%"><?= $pppsecrets ?> Account</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <center>
+                                <img class="img-thumbnail" src="{{ asset('mazer/router-hacking.webp') }}" alt=""
+                                    style="width: 100%">
+                                <br>
+                                <br>
+                                <a href=""
+                                    onclick="return confirm('Apakah anda yakin akan merestart Router <?= $identity ?> ?')"
+                                    class="d-sm-inline-block btn btn-primary shadow-sm">
+                                    <i class="fa fa-power-off fa-sm text-white-50"></i><i class="fa fa-refresh"
+                                        aria-hidden="true"></i> Reboot MikroTik Now
+                                </a>
+                                <h4 style="margin-top: 10px">{{ getRoute()->identitas_router }}</h4>
+                            </center>
                         </div>
                     </div>
                 </div>
