343027fc3882b6d475485602c9b9654253cd0c50
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-08-30 03:03:44 +0700

    update

--- resources/views/frontend/speed.blade.php
+++ resources/views/frontend/speed.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="col-md-8 offset-md-2" style="padding: 20px">
-    <div class="card">
-            <div style="text-align:right;">
+    <div class="card" style="min-height: 380px">
+            <div style="text-align:right;" >
                 <div >
                     <div style="width:100%;height:0;padding-bottom:50%;position:relative;">
                         <iframe style="border:none;position:absolute;top:0;left:0;width:100%;height:100%;min-height:360px;border:none;overflow:hidden !important;" src="//openspeedtest.com/Get-widget.php">
