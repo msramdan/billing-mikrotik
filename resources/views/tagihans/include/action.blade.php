da88daa15040109836278c0893bfde01c9055003
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-09-05 20:11:40 +0700

    update

--- resources/views/tagihans/include/action.blade.php
+++ resources/views/tagihans/include/action.blade.php
@@ -1,10 +1,9 @@
 <td>
     @can('tagihan view')
-        <a href="{{ route('tagihans.show', $model->id) }}" class="btn btn-outline-success btn-sm">
+        <a href="{{ route('tagihans.show', $model->id) }}" class="btn btn-outline-success btn-sm" title="Detail Tagihan">
             <i class="fa fa-eye"></i>
         </a>
-
-        @if ($model->status_bayar == 'Sudah Bayar')
+        {{-- @if ($model->status_bayar == 'Sudah Bayar')
             <form action="#" method="post" class="d-inline"
                 onsubmit="return confirm('Kirim Wa Notifikasi sudah bayar tagihan ?')">
                 @csrf
@@ -24,24 +23,35 @@
                     <span class="bi bi-whatsapp"></span>
                 </button>
             </form>
-        @endif
-
-        <a href="#" class="btn btn-secondary btn-sm">
+        @endif --}}
+        <a href="#" class="btn btn-secondary btn-sm"  title="Cetak Invoice">
             <i class="fa fa-print"></i>
         </a>
+
+        @if ($model->status_bayar == 'Sudah Bayar')
+            <button disabled class="btn btn-outline-success btn-sm" title="Bayar Tagihan">
+                <i class="fa fa-money-bill" aria-hidden="true"></i>
+            </button>
+        @else
+            <a href="" class="btn btn-outline-success btn-sm" title="Bayar Tagihan">
+                <i class="fa fa-money-bill" aria-hidden="true"></i>
+            </a>
+        @endif
+
+
     @endcan
     @if ($model->status_bayar == 'Sudah Bayar')
         @can('tagihan edit')
-            <button disabled class="btn btn-outline-primary btn-sm"><i class="fa fa-pencil-alt"></i></button>
+            <button disabled class="btn btn-outline-primary btn-sm" title="Edit Tagihan"><i class="fa fa-pencil-alt"></i></button>
         @endcan
         @can('tagihan delete')
             @can('tagihan edit')
-                <button disabled class="btn btn-outline-danger btn-sm"><i class="ace-icon fa fa-trash-alt"></i></button>
+                <button disabled class="btn btn-outline-danger btn-sm" title="Hapus Tagihan"><i class="ace-icon fa fa-trash-alt"></i></button>
             @endcan
         @endcan
     @else
         @can('tagihan edit')
-            <a href="{{ route('tagihans.edit', $model->id) }}" class="btn btn-outline-primary btn-sm">
+            <a href="{{ route('tagihans.edit', $model->id) }}" class="btn btn-outline-primary btn-sm" title="Edit Tagihan">
                 <i class="fa fa-pencil-alt"></i>
             </a>
         @endcan
@@ -52,7 +62,7 @@
                 @csrf
                 @method('delete')
 
-                <button class="btn btn-outline-danger btn-sm">
+                <button class="btn btn-outline-danger btn-sm" title="Hapus Tagihan">
                     <i class="ace-icon fa fa-trash-alt"></i>
                 </button>
             </form>
