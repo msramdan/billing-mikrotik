c51178e1492ed8a032aa8476cf62d81edb7e4f35
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-09-09 13:12:02 +0700

    update

--- app/Http/Requests/UpdatePelangganRequest.php
+++ app/Http/Requests/UpdatePelangganRequest.php
@@ -38,7 +38,7 @@ class UpdatePelangganRequest extends FormRequest
 			'alamat' => 'required|string',
 			'password' => 'nullable|confirmed',
 			'ppn' => 'required|in:Yes,No',
-			'status_berlangganan' => 'required|in:Aktif,Non Aktif,Menunggu',
+			'status_berlangganan' => 'required|in:Aktif,Non Aktif,Menunggu,Tunggakan',
 			'paket_layanan' => 'required|exists:App\Models\Package,id',
 			'jatuh_tempo' => 'required|numeric',
 			'kirim_tagihan_wa' => 'required|in:Yes,No',
