2e75347189a1ab4a799c3c424163ed51d7b2241e
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-12-15 14:59:12 +0700

    update

--- database/seeders/OltSeeder.php
+++ database/seeders/OltSeeder.php
@@ -17,9 +17,10 @@ class OltSeeder extends Seeder
             'company_id' => 1,
             'name' => 'OLTC320-SAWIT',
             'type' => 'zte',
-            'host' => '103.122.65.234:8161',
-            'ro' => 'sawitro',
-            'rw' => 'sawitrw',
+            'host' => '103.122.65.234',
+            'port' => '91',
+            'username' => 'sawitro',
+            'password' => 'sawitrw',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
